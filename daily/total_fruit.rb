618020228c753716730beca1b233082be50410c9
damhung0113 <hungdt1103@gmail.com>
2023-02-07 21:47:30 +0700

    [Daily] Optimal 07/02/2023

--- daily/total_fruit.rb
+++ daily/total_fruit.rb
@@ -6,22 +6,22 @@
 def total_fruit(fruits)
   n = fruits.length
   basket = Hash.new { 0 }
-  left = 0
+  left = max = 0
 
   0.upto(n - 1).each do |right|
     basket[fruits[right]] += 1
 
-    if basket.size > 2
+    while basket.size > 2
       basket[fruits[left]] -= 1
       if basket[fruits[left]] == 0
         basket.delete(fruits[left])
       end
-
-      left += 1
     end
+
+    max = [max, right - left + 1].max
   end
 
-  n - left
+  max
 end
 
 p total_fruit [1, 2, 1, 1, 1]
