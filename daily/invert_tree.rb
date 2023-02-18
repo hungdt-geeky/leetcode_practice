c7aabe401f67143ef7711810baf49520759ad563
damhung0113 <hungdt1103@gmail.com>
2023-02-18 20:54:33 +0700

    [Daily] 18/02/2023

---
+++ daily/invert_tree.rb
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+  return nil if root.nil?
+
+  left = root.left
+  right = root.right
+  root.right = invert_tree(left)
+  root.left = invert_tree(right)
+  root
+end
