62223bbe7bdddfc4243f79d77b50e879ea8c3861
damhung0113 <hungdt1103@gmail.com>
2023-02-19 11:52:03 +0700

    [Daily] 19/02/2023

---
+++ daily/zigzag_level_order.rb
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[][]}
+def zigzag_level_order(root)
+  return [] unless root
+  res = []
+  stack = [root]
+  count = 1
+  until stack.empty?
+    res << stack.map(&:val)
+    count += 1
+    arr = []
+    stack.size.times do
+      current = stack.pop
+      if count.even?
+        arr.push(current.right) if current.right
+        arr.push(current.left) if current.left
+      else
+        arr.push(current.left) if current.left
+        arr.push(current.right) if current.right
+      end
+    end
+    stack = arr
+  end
+  res
+end
